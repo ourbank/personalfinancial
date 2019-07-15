4ad22a49cdae8f9ded280df1330bdc1492e58455
limk-j <38344974+limk-j@users.noreply.github.com>
2019-07-15 15:04:43 +0800

    new version v2

--- src/main/java/com/icbc/kfzx/personalfinancial/util/DemoException.java
+++
@@ -1,7 +0,0 @@
-package com.icbc.kfzx.personalfinancial.util;
-
-public class DemoException extends Exception {
-    public DemoException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/icbc/personalfinancial/util/DemoException.java
+++
@@ -1,7 +0,0 @@
-package com.icbc.personalfinancial.util;
-
-public class DemoException extends Exception {
-    public DemoException(String message) {
-        super(message);
-    }
-}
