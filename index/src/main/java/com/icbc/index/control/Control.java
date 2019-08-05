eadaecb2fe4a7bd60e838fc0b9c91d07e17b9626
kate-chen <cgc138@163.com>
2019-08-05 16:43:03 +0800

    忽略

--- index/src/main/java/com/icbc/index/control/Control.java
+++ index/src/main/java/com/icbc/index/control/Control.java
@@ -1,23 +1,8 @@
 package com.icbc.index.control;
 
-
-import com.alibaba.fastjson.JSONObject;
-import com.icbc.index.model.CardData;
-import com.icbc.index.model.Manager;
-import com.icbc.index.service.*;
-import com.icbc.index.util.LoginUtil;
-import com.icbc.index.util.VoiceEncodeUtil;
-import com.icbc.index.util.VoiceRecognitionUtil;
-import com.icbc.index.vo.RequestMessage;
-import com.icbc.index.vo.ResponseMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 
 /*
