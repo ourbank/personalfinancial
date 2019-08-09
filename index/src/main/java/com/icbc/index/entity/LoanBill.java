37f500b772e8ae398c53fe843d3f8e0c2e937fdb
ShocnKing <shocnking@gmail.com>
2019-08-09 08:47:03 +0800

    修改实体类

--- index/src/main/java/com/icbc/index/entity/LoanBill.java
+++ index/src/main/java/com/icbc/index/entity/LoanBill.java
@@ -1,12 +1,9 @@
 package com.icbc.index.entity;
 
 
-import lombok.Data;
-
 import java.math.BigDecimal;
 import java.util.Date;
 
-@Data
 public class LoanBill {
 
     private Integer id;
@@ -14,4 +11,35 @@ public class LoanBill {
     private Date day;
     private BigDecimal num;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getBankId() {
+        return bankId;
+    }
+
+    public void setBankId(Integer bankId) {
+        this.bankId = bankId;
+    }
+
+    public Date getDay() {
+        return day;
+    }
+
+    public void setDay(Date day) {
+        this.day = day;
+    }
+
+    public BigDecimal getNum() {
+        return num;
+    }
+
+    public void setNum(BigDecimal num) {
+        this.num = num;
+    }
 }
