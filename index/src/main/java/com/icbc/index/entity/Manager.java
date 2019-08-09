a159e28af560327fac1af1217b4960137bf5101e
kate-chen <cgc138@163.com>
2019-08-09 09:02:03 +0800

    新增计划占比功能接口

--- index/src/main/java/com/icbc/index/entity/Manager.java
+++ index/src/main/java/com/icbc/index/entity/Manager.java
@@ -6,9 +6,42 @@ import java.io.Serializable;
 
 public class Manager implements Serializable {
 
-    private Integer id;
+
     private String account;
     private String password;
     private String level;
-    private Integer belongbank;
+    private Integer belongBank;
+
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public Integer getBelongBank() {
+        return belongBank;
+    }
+
+    public void setBelongBank(Integer belongBank) {
+        this.belongBank = belongBank;
+    }
 }
