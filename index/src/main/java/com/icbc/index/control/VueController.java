d211608e4acdf50ec78717045605b61beafd8433
kate-chen <cgc138@163.com>
2019-08-14 11:24:43 +0800

    添加日志

--- index/src/main/java/com/icbc/index/control/VueController.java
+++ index/src/main/java/com/icbc/index/control/VueController.java
@@ -11,12 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-public class vuecontroller {
+public class VueController {
 
     @Autowired
     BusinessSaveService businessSaveService;
